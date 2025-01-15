f1f559b8ed49a3d1a0b6d3f4eeb778671f0f233a
merliariza <180667185+merliariza@users.noreply.github.com>
2025-01-15 09:02:09 -0500

    feat: :construction: Movies pop up
    Adding descriptions for each movie

---
+++ js/movie.js
@@ -0,0 +1,25 @@
+import Swal from 'sweetalert2/dist/sweetalert2.js'
+import 'sweetalert2/src/sweetalert2.scss'
+
+
+Swal.fire({
+    title,
+    resumen,
+    imageUrl: "https://unsplash.it/400/200",
+    imageWidth: 700,
+    imageHeight: 400,
+    imageAlt: "Image"
+  });
+
+Swal.fire({
+    title,
+    description,
+    cast,
+    duration,
+    genre,
+    date,
+    imageUrl: "https://unsplash.it/400/200",
+    imageWidth: 700,
+    imageHeight: 400,
+    imageAlt: "Image"
+  });
